731a4b3b50bc1420d4a3f5bfee8b2f6a080e95b0
unknown <reuben.j.thorn@gmail.com>
2023-08-24 10:32:42 +0200

    Test Commit

--- Paradise_Point/Form1.cs
+++ Paradise_Point/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Sql;
 
 namespace Paradise_Point
 {
@@ -15,6 +16,11 @@ namespace Paradise_Point
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
