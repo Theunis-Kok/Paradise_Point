be372cf869bf264f9b6a75373ebc0f31f821b484
unknown <reuben.j.thorn@gmail.com>
2023-08-27 15:12:45 +0200

    Maintain again Emplyees

--- Paradise_Point/Maintain_Employee.cs
+++ Paradise_Point/Maintain_Employee.cs
@@ -22,10 +22,49 @@ namespace Paradise_Point
 
         
         public string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|ParadisePoint.mdf;Integrated Security=True";
+
+        String jobTitle = "";
+        String fName = "";
+        String lName = "";
+        String email = "";
+        String password = "";
         public Maintain_Employee()
         {
             InitializeComponent();
         }
+
+
+        public void Insert()
+        {
+            jobTitle = cmbJobTitle.Text;
+            fName = txtFirstName.Text;
+            lName = txtLastName.Text;
+            email = txtEmail.Text;
+            password = txtPassword.Text;
+
+            // Create a connection object
+            conn = new SqlConnection(connString);
+
+            // Open the connection
+            conn.Open();
+
+            // Create an insert command
+            cmd = new SqlCommand("INSERT INTO EMPLOYEE (jobTitle, firstName, lastName, email, password) VALUES (@jobTitle, @fName, @lName, @email, @password)", conn);
+
+            // Add the parameters
+            cmd.Parameters.AddWithValue("@jobTitle", jobTitle);
+            cmd.Parameters.AddWithValue("@fName", fName);
+            cmd.Parameters.AddWithValue("@lName", lName);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@password", password);
+
+            // Execute the command
+            cmd.ExecuteNonQuery();
+
+            // Close the connection
+            conn.Close();
+        }
+
 
         private void btnInsert_Click(object sender, EventArgs e)
         {
