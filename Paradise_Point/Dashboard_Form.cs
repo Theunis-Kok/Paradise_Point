323251487637ed7aae6201039c54e8144c235503
Theunis-Kok <theunisjkok12@gmail.com>
2023-08-31 23:46:30 +0200

    Updated the dashboard to display admin name

--- Paradise_Point/Dashboard_Form.cs
+++ Paradise_Point/Dashboard_Form.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
+using System.Data.SqlClient;
 
 namespace Paradise_Point
 {
     public partial class Dashboard_Form : Form
     {
+
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+
+        public string connString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|ParadisePoint.mdf;Integrated Security=True";
+
+
         public Dashboard_Form()
         {
             InitializeComponent();
@@ -51,6 +61,42 @@ namespace Paradise_Point
             Maintain_Employee maintain_employee = new Maintain_Employee();
             maintain_employee.ShowDialog();
             this.Hide();
+        }
+
+        private void Dashboard_Form_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(connString);
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            try
+            {
+                string query = "SELECT FirstName, LastName FROM EMPLOYEE WHERE isAdmin = 1";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string firstName = reader["FirstName"].ToString();
+                            string lastName = reader["LastName"].ToString();
+
+                            lblUserName.Text = $"Welcome, Admin {firstName} {lastName}!";
+                        }
+                        else
+                        {
+                            MessageBox.Show("No admin found.");
+                        }
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
